33ec72d445c729bba67c4845bb3b0a42bd101498
Trizalio <gifla@ya.ru>
2014-02-27 02:57:54 +0400

    first version

---
+++ Assets/stuff/scripts/bouncer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncer : MonoBehaviour {
+	public float force = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision collision){
+		var direction = collision.transform.position - transform.position;
+		direction.Normalize ();
+		direction *= force;
+		collision.rigidbody.AddForce( direction, ForceMode.VelocityChange);
+	}
+}
---
+++ Assets/stuff/scripts/fling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fling : MonoBehaviour {
+	public KeyCode Key = KeyCode.LeftArrow;
+	private float angle;
+	void Start(){
+		angle = GetComponent<HingeJoint> ().spring.targetPosition;
+		rigidbody.centerOfMass = Vector3.zero;
+	}
+
+
+	void FixedUpdate () {
+		var spring = GetComponent<HingeJoint> ().spring;
+		if (Input.GetKey (Key)) {
+			spring.targetPosition = -angle;
+		}/*else if (Input.GetKey (KeyCode.RightArrow)){
+			spring.targetPosition = -angle;
+		}*/else{
+			spring.targetPosition = angle;
+		}
+		GetComponent<HingeJoint> ().spring = spring;
+	}
+}
